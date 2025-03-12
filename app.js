cc800e91690ea90c613e4ff93008472616e49d8a
Mahesha V P <maheshavp12@gmail.com>
2025-03-12 19:44:02 +0530

    navbar changed

---
+++ app.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll(".nav-link");
+    const navbarToggler = document.querySelector(".navbar-toggler");
+    const navbarCollapse = document.querySelector(".navbar-collapse");
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            if (navbarCollapse.classList.contains("show")) {
+                navbarToggler.click(); // Programmatically clicks the toggle button to close the menu
+            }
+        });
+    });
+});
